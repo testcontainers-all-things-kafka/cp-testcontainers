9fff7b76b28766b32de4d84d949ea0b18d19ff99
Christoph Schubert <christoph.schubert@tu-dortmund.de>
2021-01-14 07:59:41 +0100

    Added more event categories to audit log test: this allows unauthorized access to be detected.

--- src/intTest/java/net/christophschubert/cp/testcontainers/AuditLogTest.java
+++ src/intTest/java/net/christophschubert/cp/testcontainers/AuditLogTest.java
@@ -28,6 +28,83 @@ public class AuditLogTest {
         userInfo.put(admin, adminSecret);
         userInfo.put("bob", "bob-secret");
 
+        final var logAllConf =
+                "{" +
+                        "        \"routes\": {" +
+                        "                  \"crn:///kafka=*\": {" +
+                        "                            \"interbroker\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"describe\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"management\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }" +
+                        "                  }," +
+                        "                  \"crn:///kafka=*/group=*\": {" +
+                        "                            \"consume\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"describe\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"management\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }" +
+                        "                  }," +
+                        "                  \"crn:///kafka=*/topic=*\": {" +
+                        "                            \"produce\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"consume\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"describe\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"management\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }" +
+                        "                  }," +
+                        "                  \"crn:///kafka=*/topic=_*\": {" +
+                        "                            \"produce\": {" +
+                        "                                      \"allowed\": \"\"," +
+                        "                                      \"denied\": \"\"" +
+                        "                            }," +
+                        "                            \"consume\": {" +
+                        "                                      \"allowed\": \"\"," +
+                        "                                      \"denied\": \"\"" +
+                        "                            }," +
+                        "                            \"describe\": {" +
+                        "                                      \"allowed\": \"\"," +
+                        "                                      \"denied\": \"\"" +
+                        "                            }" +
+                        "                  }" +
+                        "        }," +
+                        "        \"destinations\": {" +
+                        "                  \"topics\": {" +
+                        "                            \"confluent-audit-log-events\": {" +
+                        "                                      \"retention_ms\": 7776000000" +
+                        "                            }" +
+                        "                  }" +
+                        "        }," +
+                        "        \"default_topics\": {" +
+                        "                  \"allowed\": \"confluent-audit-log-events\"," +
+                        "                  \"denied\": \"confluent-audit-log-events\"" +
+                        "        }" +
+                        "}";
+
         cpServer
                 //for basic authentication
 //                        .withProperty("kafka.rest.authentication.method", "BASIC")
@@ -37,10 +114,14 @@ public class AuditLogTest {
 //                .withProperty("kafka.rest.client.sasl.jaas.config", plainJaas(admin, adminSecret))
 //                .withProperty("kafka.rest.client.security.protocol", SASL_PLAINTEXT)
 //                .withProperty("kafka.rest.client.sasl.mechanism", PLAIN)
+//
+
 
                 //audit logs capture produce/consume events
-                .withProperty("confluent.security.event.router.config","{\"routes\":{\"crn:///kafka=*/group=*\":{\"consume\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"}},\"crn:///kafka=*/topic=*\":{\"produce\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"},\"consume\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"}}},\"destinations\":{\"topics\":{\"confluent-audit-log-events\":{\"retention_ms\":7776000000}}},\"default_topics\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"},\"excluded_principals\":[]}"
-                )
+//                .withProperty("confluent.security.event.router.config","{\"routes\":{\"crn:///kafka=*/group=*\":{\"consume\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"}},\"crn:///kafka=*/topic=*\":{\"produce\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"},\"consume\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"}}},\"destinations\":{\"topics\":{\"confluent-audit-log-events\":{\"retention_ms\":7776000000}}},\"default_topics\":{\"allowed\":\"confluent-audit-log-events\",\"denied\":\"confluent-audit-log-events\"},\"excluded_principals\":[]}")
+                .withProperty("confluent.security.event.router.config", logAllConf)
+
+//                .withLogConsumer(o -> System.out.print(o))
 
                 .withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "PLAINTEXT:SASL_PLAINTEXT,BROKER:SASL_PLAINTEXT")
                 .withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER")
@@ -51,6 +132,7 @@ public class AuditLogTest {
                 .withEnv("KAFKA_SASL_JAAS_CONFIG", plainJaas(admin, adminSecret, Collections.emptyMap()))
                 .withEnv("KAFKA_LISTENER_NAME_PLAINTEXT_PLAIN_SASL_JAAS_CONFIG", plainJaas(admin, adminSecret, userInfo))
                 .withEnv("KAFKA_AUTHORIZER_CLASS_NAME", "io.confluent.kafka.security.authorizer.ConfluentServerAuthorizer")
+//                .withEnv("KAFKA_CONFLUENT_AUTHORIZER_ACCESS_RULE_PROVIDERS", "CONFLUENT")
                 .withEnv("KAFKA_SUPER_USERS", "User:" + admin);
 
         cpServer.start();
@@ -62,11 +144,11 @@ public class AuditLogTest {
         final Producer<String, String> producer = TestClients.createProducer(cpServer.getBootstrapServers(), adminJaas);
         producer.send(new ProducerRecord<>(topicName, "value")).get();
 
-
-        final TestClients.TestConsumer<String, String> consumer = TestClients.createConsumer(cpServer.getBootstrapServers(), plainJaasProperties("bob", "bob-secret"));
+        final var bobProperties = plainJaasProperties("bob", "bob-secret");
+        final TestClients.TestConsumer<String, String> consumer = TestClients.createConsumer(cpServer.getBootstrapServers(), bobProperties);
         consumer.subscribe(List.of(topicName));
         try {
-            final var values = consumer.consumeUntil(1, Duration.ofSeconds(5), 2);
+            final var values = consumer.consumeUntil(1000, Duration.ofSeconds(5), 5);
         } catch (Exception e) {
             // expect exception: bob isn't authorized to read from topic
             e.printStackTrace();
@@ -74,10 +156,19 @@ public class AuditLogTest {
             consumer.close();
         }
 
+        try {
+            final Producer<String, String> bobProducer = TestClients.createProducer(cpServer.getBootstrapServers(), bobProperties);
+            bobProducer.send(new ProducerRecord<>("bob-isnt-allowed-to-write-here", "value")).get();
+        } catch (Exception e) {
+            // expect exception: bob isn't authorized to write to topic
+            e.printStackTrace();
+        }
+
+        System.out.println("started CP server");
 
         final TestClients.TestConsumer<String, String> auditLogConsumer = TestClients.createConsumer(cpServer.getBootstrapServers(), adminJaas);
         auditLogConsumer.subscribe(List.of("confluent-audit-log-events"));
-        final var logEvents = auditLogConsumer.consumeUntil(10, Duration.ofSeconds(5), 2);
+        final var logEvents = auditLogConsumer.consumeUntil(1000, Duration.ofSeconds(2), 5);
         logEvents.forEach(System.out::println);
     }
 }
