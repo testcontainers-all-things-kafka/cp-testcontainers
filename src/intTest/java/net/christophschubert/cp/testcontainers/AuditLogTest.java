9417ca1a4a02634be14b421105ddf782200ead96
Christoph Schubert <christoph.schubert@tu-dortmund.de>
2021-01-18 07:57:02 +0100

    Added 2 audit logs config examples.

--- src/intTest/java/net/christophschubert/cp/testcontainers/AuditLogTest.java
+++ src/intTest/java/net/christophschubert/cp/testcontainers/AuditLogTest.java
@@ -1,5 +1,6 @@
 package net.christophschubert.cp.testcontainers;
 
+import net.christophschubert.cp.testcontainers.util.MdsRestWrapper;
 import net.christophschubert.cp.testcontainers.util.TestClients;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -8,9 +9,13 @@ import org.junit.Test;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 import static net.christophschubert.cp.testcontainers.SecurityConfigs.plainJaasProperties;
+import static net.christophschubert.cp.testcontainers.util.MdsRestWrapper.KafkaResourceType.Topic;
+import static net.christophschubert.cp.testcontainers.util.MdsRestWrapper.ResourceRole.ResourceOwner;
+import static net.christophschubert.cp.testcontainers.util.TestContainerUtils.startAll;
 
 public class AuditLogTest {
     @Test
@@ -135,5 +140,110 @@ public class AuditLogTest {
         logEvents.forEach(System.out::println);
     }
 
+
+    @Test
+    public void auditLogsWithMds() throws ExecutionException, InterruptedException {
+        final var factory = new CPTestContainerFactory();
+        final var ldap = factory.createLdap(Set.of("alice", "mds", "authorized", "notauthorized"));
+        final var cpServer = factory.createConfluentServer().enableRbac();
+
+        startAll(ldap, cpServer);
+        System.out.println("started CP server");
+
+        final var adminJaas = TestClients.createJaas("alice", "alice-secret");
+        var mdsWrapper = new MdsRestWrapper(cpServer.getMdsPort(), "alice", "alice-secret");
+        //should grant resource-owner as principal need rights to create previously non-existing topic
+        mdsWrapper.grantRoleOnKafkaResource("authorized", ResourceOwner, Topic, "testtopic");
+
+        final var authorizedJaas = TestClients.createJaas("authorized", "authorized-secret");
+        final var producer = TestClients.createProducer(cpServer.getBootstrapServers(), authorizedJaas);
+        producer.send(new ProducerRecord<>("testtopic", "v")).get();
+        final TestClients.TestConsumer<String, String> auditLogConsumer = TestClients.createConsumer(cpServer.getBootstrapServers(), adminJaas);
+        auditLogConsumer.subscribe(List.of("confluent-audit-log-events"));
+        final var logEvents = auditLogConsumer.consumeUntil(1000, Duration.ofSeconds(2), 5);
+        logEvents.forEach(System.out::println);
+
+    }
+
+
+    @Test
+    public void auditLogsWithMdsAdditionalConfig() throws ExecutionException, InterruptedException {
+        final var factory = new CPTestContainerFactory();
+        final var ldap = factory.createLdap(Set.of("alice", "mds", "authorized", "notauthorized"));
+        final var cpServer = factory.createConfluentServer().enableRbac();
+
+
+        final var logAllConf =
+                "{" +
+                        "        \"routes\": {" +
+                        "                  \"crn:///kafka=*\": {" +
+                        "                            \"interbroker\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"describe\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"management\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }" +
+                        "                  }," +
+                        "                  \"crn:///kafka=*/group=*\": {" +
+                        "                            \"consume\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"describe\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }," +
+                        "                            \"management\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }" +
+                        "                  }," +
+                        "                  \"crn:///kafka=*/topic=*\": {" +
+                        "                            \"produce\": {" +
+                        "                                      \"allowed\": \"confluent-audit-log-events\"," +
+                        "                                      \"denied\": \"confluent-audit-log-events\"" +
+                        "                            }" +
+                        "                  }" +
+                        "        }," +
+                        "        \"destinations\": {" +
+                        "                  \"topics\": {" +
+                        "                            \"confluent-audit-log-events\": {" +
+                        "                                      \"retention_ms\": 7776000000" +
+                        "                            }" +
+                        "                  }" +
+                        "        }," +
+                        "        \"default_topics\": {" +
+                        "                  \"allowed\": \"confluent-audit-log-events\"," +
+                        "                  \"denied\": \"confluent-audit-log-events\"" +
+                        "        }" +
+                        "}";
+
+        cpServer.withProperty("confluent.security.event.router.config", logAllConf);
+
+
+        startAll(ldap, cpServer);
+        System.out.println("started CP server");
+
+        final var adminJaas = TestClients.createJaas("alice", "alice-secret");
+        var mdsWrapper = new MdsRestWrapper(cpServer.getMdsPort(), "alice", "alice-secret");
+        //should grant resource-owner as principal need rights to create previously non-existing topic
+        mdsWrapper.grantRoleOnKafkaResource("authorized", ResourceOwner, Topic, "testtopic");
+
+        final var authorizedJaas = TestClients.createJaas("authorized", "authorized-secret");
+        final var producer = TestClients.createProducer(cpServer.getBootstrapServers(), authorizedJaas);
+        producer.send(new ProducerRecord<>("testtopic", "v")).get();
+        final TestClients.TestConsumer<String, String> auditLogConsumer = TestClients.createConsumer(cpServer.getBootstrapServers(), adminJaas);
+        auditLogConsumer.subscribe(List.of("confluent-audit-log-events"));
+        final var logEvents = auditLogConsumer.consumeUntil(1000, Duration.ofSeconds(2), 5);
+        logEvents.forEach(System.out::println);
+
+    }
 }
+
 
