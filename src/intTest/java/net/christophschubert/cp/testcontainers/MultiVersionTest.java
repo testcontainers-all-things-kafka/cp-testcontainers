a118f2747464bee707a7adf27cad91209bfd714e
Viktor Gamov <gamussa@gmail.com>
2022-04-20 01:41:53 -0400

    updated test to use parametrized test rather than hardcoded values

--- src/intTest/java/net/christophschubert/cp/testcontainers/MultiVersionTest.java
+++ src/intTest/java/net/christophschubert/cp/testcontainers/MultiVersionTest.java
@@ -2,9 +2,12 @@ package net.christophschubert.cp.testcontainers;
 
 import net.christophschubert.cp.testcontainers.util.Tag;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import io.restassured.RestAssured;
 
@@ -21,84 +24,83 @@ import static org.hamcrest.CoreMatchers.notNullValue;
  */
 public class MultiVersionTest {
 
+  private static Stream<Arguments> getCpVersions() {
     // we test against the latest bug-fix version for each of the feature versions:
-    final List<String> allTags = List.of("5.4.6", "5.5.7", "6.0.5", "6.1.4", "6.2.2", "7.0.0", "7.0.1");
+    return Stream.of(Arguments.of("5.4.6", "5.5.7", "6.0.5", "6.1.4", "6.2.2", "7.0.0", "7.0.1"));
+  }
 
-    @Test
-    public void vanillaKafka() throws InterruptedException {
-        for (final var tag : allTags) {
-            final var factory = new CPTestContainerFactory().withTag(tag);
+  @ParameterizedTest(name = "should start Apache Kafka v{0}")
+  @MethodSource("getCpVersions")
+  public void vanillaKafka(String tag) throws InterruptedException {
+    final var factory = new CPTestContainerFactory().withTag(tag);
 
-            final var kafka = factory.createKafka();
-            kafka.start();
-            Thread.sleep(3_000); //sleep to let logs accumulate
-            assertThat(kafka.getLogs().contains("INFO [KafkaServer id=1] started (kafka.server.KafkaServer)")).isTrue();
-        }
+    final var kafka = factory.createKafka();
+    kafka.start();
+    Thread.sleep(3_000); //sleep to let logs accumulate
+    assertThat(kafka.getLogs().contains("INFO [KafkaServer id=1] started (kafka.server.KafkaServer)")).isTrue();
+  }
+
+  @ParameterizedTest(name = "should start Confluent Server v{0}")
+  @MethodSource("getCpVersions")
+  public void server(String tag) throws InterruptedException {
+
+    final var factory = new CPTestContainerFactory().withTag(tag);
+
+    final var cpServer = factory.createConfluentServer();
+    cpServer.start();
+    Thread.sleep(3_000);
+    final var logs = cpServer.getLogs();
+    assertThat(logs.contains(String.format("INFO Kafka version: %s-ce", tag))).isTrue();
+    assertThat(logs.contains("INFO Kafka startTimeMs:")).isTrue();
+    assertThat(logs.contains("INFO [KafkaServer id=1] started (kafka.server.KafkaServer)")).isTrue();
+
+    RestAssured.port = cpServer.getMdsPort();
+
+    // Even without RBAC enabled we should be able to get the Kafka cluster ID as this is part of the REST proxy
+    // embedded in Confluent Server.
+    given().
+        when().
+        get("/v1/metadata/id").
+        then().
+        statusCode(200).
+        body("id", is(notNullValue()));
+  }
+
+
+  @ParameterizedTest(name = "should start container (with Kafka v{0})")
+  @MethodSource("getCpVersions")
+  public void serverWithRbac(String tag) throws InterruptedException {
+    final var featuresEndpointAddedTag = new Tag("6"); // '/security/1.0/features' endpoint was added in CP 6.0.0
+
+    final var factory = new CPTestContainerFactory().withTag(tag);
+    final var ldap = factory.createLdap();
+    final var cpServer = factory.createConfluentServer().enableRbac();
+    startAll(ldap, cpServer);
+
+    final var logs = cpServer.getLogs();
+    assertThat(logs.contains(String.format("INFO Kafka version: %s-ce", tag))).isTrue();
+    assertThat(logs.contains("INFO Kafka startTimeMs:")).isTrue();
+    assertThat(logs.contains("INFO [KafkaServer id=1] started (kafka.server.KafkaServer)")).isTrue();
+
+    RestAssured.port = cpServer.getMdsPort();
+    // Even without RBAC enabled we should be able to get the Kafka cluster ID as this is part of the REST proxy
+    // embedded in Confluent Server.
+    given().
+        when().
+        get("/v1/metadata/id").
+        then().
+        statusCode(200).
+        body("id", is(notNullValue(String.class)));
+
+    
+    if (new Tag(tag).atLeast(featuresEndpointAddedTag)) {
+      //check whether RBAC is enabled
+      given().
+          when().
+          get("/security/1.0/features").
+          then().
+          statusCode(200).
+          body("features.'basic.auth.1.enabled'", is(true));
     }
-
-
-    @Test
-    public void server() throws InterruptedException {
-        for (final var tag : allTags) {
-            final var factory = new CPTestContainerFactory().withTag(tag);
-
-            final var cpServer = factory.createConfluentServer();
-            cpServer.start();
-            Thread.sleep(3_000);
-            final var logs = cpServer.getLogs();
-            assertThat(logs.contains(String.format("INFO Kafka version: %s-ce", tag))).isTrue();
-            assertThat(logs.contains("INFO Kafka startTimeMs:")).isTrue();
-            assertThat(logs.contains("INFO [KafkaServer id=1] started (kafka.server.KafkaServer)")).isTrue();
-
-            RestAssured.port = cpServer.getMdsPort();
-
-            // Even without RBAC enabled we should be able to get the Kafka cluster ID as this is part of the REST proxy
-            // embedded in Confluent Server.
-            given().
-                    when().
-                    get("/v1/metadata/id").
-                    then().
-                    statusCode(200).
-                    body("id", is(notNullValue()));
-        }
-    }
-
-
-    @Test
-    public void serverWithRbac() throws InterruptedException {
-        final var featuresEndpointAddedTag = new Tag("6"); // '/security/1.0/features' endpoint was added in CP 6.0.0
-
-        for (final var tag : allTags) {
-            final var factory = new CPTestContainerFactory().withTag(tag);
-            final var ldap = factory.createLdap();
-            final var cpServer = factory.createConfluentServer().enableRbac();
-            startAll(ldap, cpServer);
-
-            final var logs = cpServer.getLogs();
-            assertThat(logs.contains(String.format("INFO Kafka version: %s-ce", tag))).isTrue();
-            assertThat(logs.contains("INFO Kafka startTimeMs:")).isTrue();
-            assertThat(logs.contains("INFO [KafkaServer id=1] started (kafka.server.KafkaServer)")).isTrue();
-
-            RestAssured.port = cpServer.getMdsPort();
-            // Even without RBAC enabled we should be able to get the Kafka cluster ID as this is part of the REST proxy
-            // embedded in Confluent Server.
-            given().
-                    when().
-                    get("/v1/metadata/id").
-                    then().
-                    statusCode(200).
-                    body("id", is(notNullValue(String.class)));
-
-
-            if (new Tag(tag).atLeast(featuresEndpointAddedTag)) {
-                //check whether RBAC is enabled
-                given().
-                        when().
-                        get("/security/1.0/features").
-                        then().
-                        statusCode(200).
-                        body("features.'basic.auth.1.enabled'", is(true));
-            }
-        }
-    }
+  }
 }
