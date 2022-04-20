3353cc32e430ba1dc6b47768e7397bf6c6d0882f
Viktor Gamov <gamussa@gmail.com>
2022-04-20 02:05:22 -0400

    fix multiversion test

--- src/intTest/java/net/christophschubert/cp/testcontainers/MultiVersionTest.java
+++ src/intTest/java/net/christophschubert/cp/testcontainers/MultiVersionTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 
 import java.util.stream.Stream;
 
@@ -26,7 +29,7 @@ public class MultiVersionTest {
 
   private static Stream<Arguments> getCpVersions() {
     // we test against the latest bug-fix version for each of the feature versions:
-    return Stream.of(Arguments.of("5.4.6", "5.5.7", "6.0.5", "6.1.4", "6.2.2", "7.0.0", "7.0.1"));
+    return Stream.of(Arguments.of("5.4.6"), Arguments.of("5.5.7"), Arguments.of("6.0.5"), Arguments.of("6.1.4"), Arguments.of("6.2.2"), Arguments.of("7.0.0"), Arguments.of("7.0.1"));
   }
 
   @ParameterizedTest(name = "should start Apache Kafka v{0}")
@@ -35,8 +38,9 @@ public class MultiVersionTest {
     final var factory = new CPTestContainerFactory().withTag(tag);
 
     final var kafka = factory.createKafka();
+    kafka.waitingFor(Wait.forLogMessage("INFO [KafkaServer id=1] started",1));
     kafka.start();
-    Thread.sleep(3_000); //sleep to let logs accumulate
+    //Thread.sleep(3_000); //sleep to let logs accumulate
     assertThat(kafka.getLogs().contains("INFO [KafkaServer id=1] started (kafka.server.KafkaServer)")).isTrue();
   }
 
@@ -47,8 +51,9 @@ public class MultiVersionTest {
     final var factory = new CPTestContainerFactory().withTag(tag);
 
     final var cpServer = factory.createConfluentServer();
+    cpServer.waitingFor(Wait.forLogMessage("INFO [KafkaServer id=1] started",1));
     cpServer.start();
-    Thread.sleep(3_000);
+    //Thread.sleep(3_000);
     final var logs = cpServer.getLogs();
     assertThat(logs.contains(String.format("INFO Kafka version: %s-ce", tag))).isTrue();
     assertThat(logs.contains("INFO Kafka startTimeMs:")).isTrue();
@@ -92,7 +97,6 @@ public class MultiVersionTest {
         statusCode(200).
         body("id", is(notNullValue(String.class)));
 
-    
     if (new Tag(tag).atLeast(featuresEndpointAddedTag)) {
       //check whether RBAC is enabled
       given().
